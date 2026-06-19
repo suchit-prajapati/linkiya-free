b4fb6cc5679dd7132b13585b2b3390e1359a3a75
Suchit Prajapati <prajapatisuchit2@gmail.com>
2026-06-20 00:29:57 +0530

    Sync sidebar.asset.php

--- build/sidebar.asset.php
+++ build/sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '99e3568544ed6ca2e97a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'a24cd21f3f95120424dc');
